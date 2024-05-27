efe0603f4fa93d15e2978450af7a29b81eb2d79b
Alex Reardon <alexreardon@gmail.com>
2024-05-27 16:43:37 +1000

    fixing tsup issue with cjs build

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   tsconfig: './tsconfig.json',
+  plugins: [
+    {
+      // https://github.com/egoist/tsup/issues/953#issuecomment-2132576167
+      // ensuring that all local requires in `.cjs` files import from `.cjs` files.
+      // require('./path') → require('./path.cjs') in `.cjs` files
+      name: 'fix-cjs-require',
+      renderChunk(_, { code }) {
+        if (this.format === 'cjs') {
+          const regex = /require\("(?<import>\.\/.+)"\)/g;
+          // TODO: should do nothing if file already ends in .cjs
+          // TODO: could be more resilient for `"` vs `'` imports
+          return { code: code.replace(regex, "require('$<import>.cjs')") };
+        }
+      },
+    },
+  ],
 });
